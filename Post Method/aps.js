fea6d46a1a544fa983b1a651234a6a2c3fb16803
firatdemir47 <98283697+firatdemir47@users.noreply.github.com>
2025-08-16 18:49:58 +0300

    Add basic routing, POST handling, and HTML page serving for Node.js server

---
+++ Post Method/aps.js
@@ -0,0 +1,75 @@
+var http = require("http");
+var fs = require("fs");
+
+var server = http.createServer((request, response) => {
+
+    if (request.url == '/') {
+        fs.readFile(__dirname + "/indexs.html", (error, html) => {
+            if (error) {
+                response.writeHead(500, {"Content-Type": "text/plain"});
+                response.write("indexs.html dosyası okunamadı!");
+                response.end();
+            } else {
+                response.writeHead(200, {"Content-Type": "text/html"});
+                response.write(html);
+                response.end();
+            }
+        });
+    }
+    else if (request.url == "/blogss") {
+        fs.readFile(__dirname + "/blogss.html", (error, html) => {
+            if (error) {
+                response.writeHead(500, {"Content-Type": "text/plain"});
+                response.write("blogss.html dosyası okunamadı!");
+                response.end();
+            } else {
+                response.writeHead(200, {"Content-Type": "text/html"});
+                response.write(html);
+                response.end();
+            }
+        });
+    }
+    else if (request.url == "/create" && request.method=="POST") {
+        fs.appendFile("blogs.txt","deneme",(err)=>{
+            if(err){
+                console.log(err);
+            }
+            else{
+                response.statusCode=302;
+                response.setHeader("Location","/");
+                response.end();
+            }
+        })
+    }
+    else if (request.url == "/create") {
+        fs.readFile(__dirname + "/create.html", (error, html) => {
+            if (error) {
+                response.writeHead(500, {"Content-Type": "text/plain"});
+                response.write("create.html dosyası okunamadı!");
+                response.end();
+            } else {
+                response.writeHead(200, {"Content-Type": "text/html"});
+                response.write(html);
+                response.end();
+            }
+        });
+    }
+    
+    else {
+        fs.readFile(__dirname + "/404s.html", (error, html) => {
+            if (error) {
+                response.writeHead(500, {"Content-Type": "text/plain"});
+                response.write("404s.html dosyası okunamadı!");
+                response.end();
+            } else {
+                response.writeHead(404, {"Content-Type": "text/html"});
+                response.write(html);
+                response.end();
+            }
+        });
+    }
+
+});
+
+server.listen(3000);
+console.log("node.js server at port 3000");
